deaf35afc6d76e6a7ec5fb5d9a44359c08bc488f
Vasyl Protsukovych <protcykovich@gmail.com>
2024-05-26 01:00:04 +0200

    Push new structure

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,30 @@
-import { defineConfig } from 'vite'
-import { resolve } from 'path'
+import { defineConfig } from 'vite';
+import glob from 'glob';
+import injectHTML from 'vite-plugin-html-inject';
+import FullReload from 'vite-plugin-full-reload';
 
-export default defineConfig({
-  base: '/goit-advancedjs-hw-01/',
-  build: {
-    rollupOptions: {
-      input: {
-        mainPage: resolve(__dirname, 'index.html'),
-        galleryPage: resolve(__dirname, 'src/01-gallery.html'),
-        videoPage: resolve(__dirname, 'src/02-video.html'),
-        feedbackPage: resolve(__dirname, 'src/03-feedback.html')
+export default defineConfig(({ command }) => {
+  return {
+    define: {
+      [command === 'serve' ? 'global' : '_global']: {},
+    },
+    root: 'src',
+    build: {
+      sourcemap: true,
+
+      rollupOptions: {
+        input: glob.sync('./src/*.html'),
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return 'vendor';
+            }
+          },
+          entryFileNames: 'commonHelpers.js',
+        },
       },
-      output: {
-        dir: 'dist',
-        assetFileNames: (assetInfo) => {
-          return `assets/[name].[ext]`;
-        }
-      }
-    }
-  }
-})
+      outDir: '../dist',
+    },
+    plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
+  };
+});
