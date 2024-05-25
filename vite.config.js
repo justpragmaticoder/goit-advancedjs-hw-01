a2632e90bd107cbcf4926adbe70a2c8f86c113d6
Vasyl Protsukovych <protcykovich@gmail.com>
2024-05-25 23:08:11 +0200

    Small improvements for vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,19 @@
-import { defineConfig } from 'vite'
-import { resolve } from 'path'
+import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
+  base: "/goit-advancedjs-hw-01/",
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        gallery: resolve(__dirname, 'src/01-gallery.html'),
-        video: resolve(__dirname, 'src/02-video.html'),
-        feedback: resolve(__dirname, 'src/03-feedback.html')
+        main: resolve(__dirname, "index.html"),
+        gallery: resolve(__dirname, "src/01-gallery.html"),
+        video: resolve(__dirname, "src/02-video.html"),
+        feedback: resolve(__dirname, "src/03-feedback.html"),
       },
       output: {
-        dir: 'dist',
-      }
-    }
-  }
-})
+        dir: "dist",
+      },
+    },
+  },
+});
