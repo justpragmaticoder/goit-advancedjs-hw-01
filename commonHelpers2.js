750c5c8758b1f15de01fb16d4b76e5e7f0883041
justpragmaticoder <protcykovich@gmail.com>
2024-05-25 23:19:57 +0000

    Deploying to gh-pages from @ justpragmaticoder/goit-advancedjs-hw-01@440322f19a1b7e57081579cf75a17a1d33164bb5 🚀

--- commonHelpers2.js
+++
@@ -1,2 +0,0 @@
-import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */import{P as r,t as o}from"./assets/vendor-1e54b975.js";function a(e){localStorage.setItem("videoplayer-current-time",e.seconds)}(()=>{const e=document.querySelector("iframe"),t=new r(e);t.setCurrentTime(localStorage.getItem("videoplayer-current-time")??0),t.on("timeupdate",o(a,1e3))})();
-//# sourceMappingURL=commonHelpers2.js.map
